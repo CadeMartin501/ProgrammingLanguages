20a67c44fb639755f5799447a3e500404206ff15
cadeemartin <caderade88@gmail.com>
2025-04-18 13:50:58 -0500

    in class

--- Ruby/frequency.rb
+++ Ruby/frequency.rb
@@ -2,10 +2,15 @@ def clean_word(word)
   word.downcase
 end
 
+
+freqCount = Hash.new(0)
 line = gets
 while line
   line.split.each do |word|
-    puts word
+    freqCount[word] += 1
   end
   line = gets
 end
+
+puts freqCount.sort_by {|_, b| -b}[..10]
+
